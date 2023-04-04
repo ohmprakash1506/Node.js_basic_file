a71150df9ff0cc8a42bac2fa60c3b7ccd03fa6f5
ohmprakash <ohmprakash96@outlook.com>
2023-04-04 16:44:51 +0530

    added file_system.js file and text.txt

---
+++ file_system.js
@@ -0,0 +1,28 @@
+var fs = require("fs"); //* --> creating file system
+
+//*reading a file
+fs.readFile("input.txt", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log(data.toString());
+});
+
+//*writing a file
+fs.writeFile("test.txt", "Hello world !", (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Content is written in test.txt`);
+  }
+});
+
+//*appeneding content to existing file
+
+fs.appendFile("test.txt", "\n hello there again!", (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Content has been appended to test.txt`);
+  }
+});
